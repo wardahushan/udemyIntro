ef09ecbb164a615612e4389eeb9c23fcc8d6c44a
wardahushan <ushwar001@myuct.ac.za>
2018-09-14 00:33:46 +0200

    Solved Exercises for Lecture 49

---
+++ Udemy/Lecture43.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Udemy
+{
+    public class Lecture43
+    {
+        public static void RunLecture43()
+        {
+            // Header section 
+            StringBuilder header = new StringBuilder();
+            header.AppendLine("----------------------").AppendLine("LECTURE 43 EXERCISES").AppendLine("----------------------");
+            Console.Write(header);
+
+            // Lecture 43: Problem 1 
+            Console.WriteLine("Problem 1:\nEnter (float) value between 1 and 10");
+            float.TryParse(Console.ReadLine(), out float value);
+
+            if (value >= 1 && value <= 10)
+            {
+                Console.WriteLine($"You provided value: {value}. Valid.\n");
+            }
+            else
+            {
+                Console.WriteLine("You provided an invalid value. Moron. It's invalid.\n");
+            }
+
+            // Lecture 43: Problem 2 
+            Console.WriteLine("Problem 2:\nEnter first number");
+            float first = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            float second = float.Parse(Console.ReadLine());
+
+            float maxNumber = first > second ? first : second;
+            Console.WriteLine($"The maximum of the two numbers is {maxNumber}\n");
+
+
+            // Lecture 43: Problem 3
+            Console.WriteLine("Problem 3:\nEnter height");
+            float height = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter width");
+            float width = float.Parse(Console.ReadLine());
+
+            string pictureSize = height > width ? "portrait" : "landscape";
+            Console.WriteLine($"The picture size is {pictureSize}\n");
+
+            // Lecture 43: Problem 4
+            Console.WriteLine("Problem 4:\nEnter speed limit");
+            float limit = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter current speed");
+            float speed = float.Parse(Console.ReadLine());
+
+            if (speed <= limit)
+            {
+                Console.WriteLine("OK\n");
+            }
+            else
+            {
+                int demerit = DetermineDemeritPoints(limit, speed);
+
+                if (demerit > 12)
+                {
+                    Console.WriteLine($"Demerit points are: {demerit}. License suspended.\n");
+                }
+                else
+                {
+                    Console.WriteLine($"Demerit points are: {demerit}. Be careful - if points exceed 12, license may be suspended.\n");
+                }
+            }
+        }
+
+        public static int DetermineDemeritPoints(float limit, float speed)
+        {
+            int demeritPoints = (int)(speed - limit) / 5;
+            return demeritPoints;
+        }
+    }
+}
---
+++ Udemy/Lecture49.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Udemy
+{
+    public class Lecture49
+    {
+        internal static void RunLecture49()
+        {
+            // Header section 
+            StringBuilder header = new StringBuilder();
+            header.AppendLine("----------------------").AppendLine("LECTURE 49 EXERCISES").AppendLine("----------------------");
+            Console.Write(header);
+
+            // Lecture 49: Problem 1 
+            int startNum = 1;
+            int endNum = 100;
+            int count = 0;
+
+            for (int i = startNum; i <= endNum; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine($"Problem 1:\nThere are {count} numbers between 1 and 100 that are divisible by 3.\n");
+
+            // Lecture 49: Problem 2
+            double total = 0;
+            Console.WriteLine("Problem 2:\n");
+
+            while (true)
+            {
+                Console.WriteLine("Enter a number to sum to total, or enter ok to quit.");
+                string response = Console.ReadLine();
+                if (response.Trim().ToLower() == "ok")
+                {
+                    Console.WriteLine($"The sum of the numbers you entered is: {total}");
+                    break;
+                }
+                else
+                {
+                    total += double.Parse(response);
+                }
+            }
+
+            // Lecture 49: Problem 3 
+            double factorial = 1;
+            Console.WriteLine("\nProblem 3:\nEnter a number for which to calculate the factorial.");
+            double maxNumber = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= maxNumber; i++)
+            {
+                factorial = factorial * i;
+            }
+
+            Console.WriteLine($"The factorial of the number is {factorial}.");
+
+            // Lecture 49: Problem 4
+            Console.WriteLine("\nProblem 4:\nGuess what the random lottery number is! Hint: something between 1 and 10. You get upto 4 chances.");
+            Random random = new Random();
+            int randomNumber = random.Next(1, 10);
+            int chances = 1;
+            bool responseCorrect = false;
+
+            Console.WriteLine("Make a guess:");
+
+            do
+            {
+                responseCorrect = Int32.Parse(Console.ReadLine()) == randomNumber;
+                chances++;
+
+                if (responseCorrect == true)
+                {
+                    Console.WriteLine("Well done! You guessed the correct number!");
+                    break;
+                }
+
+                Console.WriteLine("Wrong answer.");
+
+            } while (chances <= 4);
+
+            if (responseCorrect == false)
+            {
+                Console.WriteLine("Sorry - no prize. You used all your chances.");
+            }
+
+            // Lecture 49: Problem 5
+            Console.WriteLine("\nProblem 5:\nEnter a series of numbers seperated by a comma, e.g \"5, 3, 8, 1, 4\".");
+            string numbersList = Console.ReadLine();
+            double maximum = numbersList.Split(',').Select(x => double.Parse(x)).ToList().Max();
+            Console.WriteLine($"The largest number you entered is {maximum}.\n");
+
+        }
+    }
+}
